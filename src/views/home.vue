d3fbd76601bb2d5d06e6c5acf2e35e3e705d19c7
TB8466 <thorbjornbros@gmail.com>
2022-12-21 15:15:41 +0100

    init

---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center flex justify-center">
+    <div class="border-2 py-10 px-20 h-screen">
+        <img class="m-auto w-80" src="../assets/img/VIASOL_LOGO.png" alt="Logo missing">
+        <h2 class="text-xl font-semibold">Velkommen til Viasol support</h2>
+        <p>For at oprette en support henvendelse, venligst udfyld telefonnummer og find dig selv på listen:</p>
+        <form class="p-4">
+            <label for="phone">Telefonnummer:</label>
+            <input class="border ml-2" type="text" name="phone" id="phone">
+        </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
