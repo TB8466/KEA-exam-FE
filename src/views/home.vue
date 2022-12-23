2cdaa6e422ba24b33beb7c2c66e62a2d8ed36efa
TB8466 <thorbjornbros@gmail.com>
2022-12-23 11:19:48 +0100

    GetContactByQuery, Careful of Rate-Limit / Secondly-limit

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,17 +4,41 @@
         <img class="m-auto w-80" src="../assets/img/VIASOL_LOGO.png" alt="Logo missing">
         <h2 class="text-xl font-semibold">Velkommen til Viasol support</h2>
         <p>For at oprette en support henvendelse, venligst udfyld telefonnummer og find dig selv på listen:</p>
-        <form class="p-4">
+            
             <label for="phone">Telefonnummer:</label>
-            <input class="border ml-2" type="text" name="phone" id="phone">
-        </form>
+            <input v-model="input" @input="sendPhoneNumber(this.input)" class="border ml-2" type="text" name="phone" id="phone">
+
+            <br>
+            <br>
+
+            <select v-if="input" v-model="custID" class="h-10 border">
+              <option v-for="contact in contacts" :key="contact.id" :value="contact.id">{{ contact.properties.firstname }} {{ contact.properties.lastname }}</option>
+            </select>
+            <br>
+            <p v-if="custID">CustomerID : {{ custID }}</p>
     </div>
+    
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      input : "",
+      contacts: [],
+      custID : ""
+    }
+  },
+  methods : {
+     sendPhoneNumber(number){
+      console.log("Number send");
+       fetch(`http://localhost:5000/contact/${number}`)
+      .then(res => res.json())
+      .then(data => this.contacts = data)
+      .catch(err => console.log(err.message))
+    }
+  }
 }
 </script>
 
