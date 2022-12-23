4228ef05944890487faae1a49f0c3893f59e5935
TB8466 <thorbjornbros@gmail.com>
2022-12-23 14:00:03 +0100

    getDealsByContactId

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,16 +6,21 @@
         <p>For at oprette en support henvendelse, venligst udfyld telefonnummer og find dig selv på listen:</p>
             
             <label for="phone">Telefonnummer:</label>
-            <input v-model="input" @input="sendPhoneNumber(this.input)" class="border ml-2" type="text" name="phone" id="phone">
+            <input v-model="input" @input="findCustomer(this.input)" class="border ml-2" type="text" name="phone" id="phone">
 
             <br>
             <br>
 
-            <select v-if="input" v-model="custID" class="h-10 border">
+            <select @change="findDeals(custID)" v-if="input" v-model="custID" class="h-10 border">
               <option v-for="contact in contacts" :key="contact.id" :value="contact.id">{{ contact.properties.firstname }} {{ contact.properties.lastname }}</option>
             </select>
             <br>
-            <p v-if="custID">CustomerID : {{ custID }}</p>
+            <p v-if="custID">Dit kundenr er : {{ custID }}</p>
+            <br>
+            <p v-if="custID">Vælg hvilken sag der drejer sig om:</p>
+            <select v-if="custID" class="h-10 border">
+              <option v-for="deal,index in deals" :key="index">{{ deal.id }}</option>
+            </select>
     </div>
     
   </div>
@@ -26,16 +31,24 @@ export default {
   data() {
     return {
       input : "",
-      contacts: [],
+      contacts : [],
+      deals : [],
       custID : ""
     }
   },
   methods : {
-     sendPhoneNumber(number){
+     findCustomer(number){
       console.log("Number send");
-       fetch(`http://localhost:5000/contact/${number}`)
+       fetch(`http://localhost:5000/contacts/${number}`)
       .then(res => res.json())
       .then(data => this.contacts = data)
+      .catch(err => console.log(err.message))
+    },
+    findDeals(contactId){
+      console.log("contactId send");
+      fetch(`http://localhost:5000/deals/${contactId}`)
+      .then(res => res.json())
+      .then(data => this.deals = data.results[0].to)
       .catch(err => console.log(err.message))
     }
   }
